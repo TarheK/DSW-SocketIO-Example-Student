d27eef37dbbc1a6390b5f9f88cd94b2ed5b69d3c
P3 <ortizadrian628@gmail.com>
2019-05-07 11:10:21 -0700

    hey

--- webapp.py
+++ webapp.py
@@ -15,13 +15,20 @@ thread_lock = Lock() #we'll use this lock to prevent multiple clients from modif
 def index():
     return render_template('home.html', async_mode=socketio.async_mode)
 
-@Socketio.on('connect')
+def background_thread():
+    count = 0
+    while True:
+        socketio.sleep(5) #waits 5 seconds
+        count=count+1
+        sockrtio.emit('count_event', count) #send count to all clients
+
+@socketio.on('connect')
 def test_connect():
-       # global thread # using the thread variable created at the top of the code
-#with thrad_lock: #lock the thread so other clients can't change it 
-   # if thread is None:
-       # thread=socketio.start_background_task(target=background_thread)
-emit('start', 'connected')#send message to client
+    global thread # using the thread variable created at the top of the code
+    with thrad_lock: #lock the thread so other clients can't change it 
+         if thread is None:
+            thread=socketio.start_background_task(target=background_thread)
+    emit('start', 'connected')#send message to client
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
